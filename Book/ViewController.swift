9b69213fa43f91522ff7f0491d11df627e6679f9
Horace Ho <horaceh@gmail.com>
2022-10-24 14:03:13 +0800

    Notify changes for debug

--- Book/ViewController.swift
+++ Book/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var pdfView: PDFView?
 
+    @IBOutlet var infoButton: UIView!
     @IBOutlet var menuButton: UIView!
     @IBOutlet var prevButton: UIView!
     @IBOutlet var nextButton: UIView!
@@ -69,17 +70,47 @@ class ViewController: UIViewController {
             object: nil
         )
 
-        let doubleTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleDoubleTap(sender:)))
-        doubleTap.numberOfTapsRequired = 2
-        pdfView?.addGestureRecognizer(doubleTap)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleDocumentChanged(notification:)),
+            name: Notification.Name.PDFViewDocumentChanged,
+            object: nil
+        )
 
-        let singleTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleSingleTap(sender:)))
-        singleTap.numberOfTapsRequired = 1
-        singleTap.require(toFail: doubleTap)
-        pdfView?.addGestureRecognizer(singleTap)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handlePageChanged(notification:)),
+            name: Notification.Name.PDFViewPageChanged,
+            object: nil
+        )
 
-        doubleTap.delaysTouchesBegan = true
-        singleTap.delaysTouchesBegan = true
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleDisplayBoxChanged(notification:)),
+            name: Notification.Name.PDFViewDisplayBoxChanged,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleScaleChanged(notification:)),
+            name: Notification.Name.PDFViewScaleChanged,
+            object: nil
+        )
+
+        if let pdf = pdfView {
+            let doubleTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleDoubleTap(sender:)))
+            doubleTap.numberOfTapsRequired = 2
+            pdf.addGestureRecognizer(doubleTap)
+
+            let singleTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleSingleTap(sender:)))
+            singleTap.numberOfTapsRequired = 1
+            singleTap.require(toFail: doubleTap)
+            pdf.addGestureRecognizer(singleTap)
+
+            doubleTap.delaysTouchesBegan = true
+            singleTap.delaysTouchesBegan = true
+        }
     }
 
     @objc func handleSingleTap(sender: AnyObject?) {
@@ -88,6 +119,36 @@ class ViewController: UIViewController {
 
     @objc func handleDoubleTap(sender: AnyObject?) {
         pdfView?.goToPreviousPage(nil)
+    }
+
+    @objc func handleDocumentChanged(notification: Notification) {
+        if let pdf = pdfView {
+            if let doc = pdf.document {
+                if let url = doc.documentURL {
+                    debugPrint("handleDocumentChanged: \(url)")
+                }
+            }
+        }
+    }
+
+    @objc func handlePageChanged(notification: Notification) {
+        if let pdf = pdfView {
+            if let page = pdf.currentPage {
+                debugPrint("handlePageChanged: \(page)")
+            }
+        }
+    }
+
+    @objc func handleDisplayBoxChanged(notification: Notification) {
+        if let pdf = pdfView {
+            debugPrint("handleDisplayBoxChanged: \(pdf.displayBox)")
+        }
+    }
+
+    @objc func handleScaleChanged(notification: Notification) {
+        if let pdf = pdfView {
+            debugPrint("handleScaleChanged: \(pdf.scaleFactor)")
+        }
     }
 
     @objc func openUrl(notification: Notification) {
@@ -112,6 +173,10 @@ class ViewController: UIViewController {
     @objc func handleActive() {
         print("handleActive")
         updateSettings()
+    }
+
+    @IBAction func tapInsideInfoButton() {
+        debugPrintAll()
     }
 
     @IBAction func tapInsideMenuButton() {
@@ -168,7 +233,33 @@ class ViewController: UIViewController {
             self.setNeedsStatusBarAppearanceUpdate()
 
             let alpha = UserDefaults.standard.bool(forKey: "dimMenuButton") ? 0.2 : 1.0
+            self.infoButton.alpha = 0.1
             self.menuButton.alpha = alpha
         })
+    }
+
+    func debugPrintAll()
+    {
+        if let pdf = pdfView {
+            debugPrint(pdf)
+            if let doc = pdf.document {
+                if let url = doc.documentURL {
+                    debugPrint("handleDocumentChanged: \(url)")
+                }
+            }
+            debugPrint("isUsingPageViewController: \(pdf.isUsingPageViewController)")
+            debugPrint("backgroundColor: \(pdf.backgroundColor)")
+            debugPrint("displaysPageBreaks: \(pdf.displaysPageBreaks)")
+            debugPrint("displayDirection: \(pdf.displayDirection)")
+            debugPrint("displayMode: \(pdf.displayMode)")
+            debugPrint("displayBox: \(pdf.displayBox)")
+            debugPrint("displaysRTL: \(pdf.displaysRTL)")
+            debugPrint("scaleFactor: \(pdf.scaleFactor)")
+            debugPrint("autoScales: \(pdf.autoScales)")
+            if let page = pdf.page(for: CGPointZero, nearest: true) {
+                let rect = pdf.convert(pdf.bounds, to:page)
+                debugPrint("viewBounds: \(pdf.bounds) converted: \(rect)")
+            }
+        }
     }
 }
